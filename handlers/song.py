c0c77406ed0c163055f29031b64bf2656cf85a70
Nuraplays <83187477+Nuraplays@users.noreply.github.com>
2021-05-09 03:58:20 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -25,7 +25,6 @@ from helpers.errors import DurationLimitError
 def song(client, message):
 
     user_id = message.from_user.id 
-    requested_by = message_.from_user.first_name
     user_name = message.from_user.first_name 
     rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
 
@@ -33,7 +32,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song🤔...')
+    m = message.reply('🔎 Finding the song 🤔...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -62,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by 𝙉𝙪𝙧𝙖 𝙙𝙖𝙧𝙡𝙞𝙣𝙜 🖤 **'.
+        rep = '**🎵 Uploaded by 𝙉𝙪𝙧𝙖 𝙙𝙖𝙧𝙡𝙞𝙣𝙜 🖤 **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
