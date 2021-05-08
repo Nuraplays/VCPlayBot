f26b4c6722034342a25bc4000304de89df35cd3f
Nuraplays <83187477+Nuraplays@users.noreply.github.com>
2021-05-09 01:04:50 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -21,7 +21,7 @@ import sys
 import time
 from helpers.errors import DurationLimitError
 
-@Client.on_message(filters.command('song') & ~filters.channel)
+@Client.on_message(filters.command('uta') & ~filters.channel)
 def song(client, message):
 
     user_id = message.from_user.id 
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by Nura darling 😘❤️ **'
+        rep = '🎵 Uploaded by 𝙉𝙪𝙧𝙖 𝙙𝙖𝙧𝙡𝙞𝙣𝙜 on the request of {}'.format(message.from_user.id)
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
