ff15a9884582c6c0b1652b773a3d869f47ba8dda
Nuraplays <83187477+Nuraplays@users.noreply.github.com>
2021-05-20 23:48:40 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -86,7 +86,7 @@ async def help(client: Client, message: Message):
 /splay <song name> - play song you requested via jio saavn
 /playlist - Show now playing list
 /current - Show now playing
-/song <song name> - download songs you want quickly
+/sunle <song name> - download songs you want quickly
 /search <query> - search videos on youtube with details
 /deezer <song name> - download songs you want quickly via deezer
 /saavn <song name> - download songs you want quickly via saavn
